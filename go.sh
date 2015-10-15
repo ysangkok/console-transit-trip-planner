4c0c2410c5cf640e7a1d7dd83e45fab0804c676b
Janus <ysangkok@gmail.com>
2015-10-15 14:22:38 +0200

    use gradle for dependency management

--- go.sh
+++ go.sh
@@ -1,13 +1,7 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 set -x
 set -o nounset
 set -o pipefail
 set -e
-if [ ! -e enabler/build/libs/enabler.jar ]; then
-  if [ ! -e build.gradle ]; then
-    wget https://github.com/schildbach/public-transport-enabler/archive/master.tar.gz
-    tar --strip-components=1 -zxvf master.tar.gz
-  fi
-  gradle jar
-fi
-groovy -cp enabler/build/libs/enabler.jar:$(echo $HOME/.gradle/{caches/{artifacts-23/filestore/net.sf.kxml/kxml2/2.3.0/jar/**/kxml2-2.3.0.jar,jars-1/**/guava-jdk5-17.0.jar,modules-2/files-2.1/{org.json/json/20090211/**/json-20090211.jar,org.slf4j/slf4j-api/1.7.12/**/slf4j-api-1.7.12.jar,com.google.guava/guava/18.0/**/guava-18.0.jar}},wrapper/dists/gradle-2.1-all/**/gradle-2.1/lib/{logback-classic-1.0.13.jar,logback-core-1.0.13.jar}} | tr ' ' ':') opnv.groovy "$@"
+cd cmdlet
+gradle runScript '-PscriptArgs=Bahn,DA Taunusplatz,Vladivostok'
